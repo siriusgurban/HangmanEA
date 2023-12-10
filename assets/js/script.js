26c2021e505a0bec9f13f3cd614f9fa5506ff0b6
2883857@student.edu.az <2883857@student.edu.az>
2023-12-11 03:59:36 +0400

    basic hangman is done, works

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,54 +18,54 @@ let randomNumber = Math.floor(Math.random() * words.length);
 
 function startGame(word) {
     audioElement.play();
-    let findWord = [];
     let attemp = 0;
+    let findWord = [];
 
     for (let i = 0; i < word.length; i++) {
         findWord.push(".");
     }
     wordId.innerHTML = findWord.join("");
 
-    addEventListener("keypress", (e) => {//event when I push the keys
-
+    addEventListener("keypress", function func(e) {//event when I push the keys
+        console.log(attemp);
         if (word.includes(e.key)) {
-
-
 
             for (let i = 0; i < word.length; i++) {
                 if (word[i] === e.key) findWord.splice(i, 1, e.key); //changing dots(".") with correct letters
+            }
 
+            let bool = findWord.every((el) => {// checking for, if word completed than invoke the winner() function
+                if (el !== ".")
+                    return true;
+            })
+
+            if (bool && attemp <= 4) {
+
+                removeEventListener('keypress', func);// remove function to not let gameover overlay with winner() 
+                winner();
 
             }
-            
-            console.log(findWord.every((el) => { el !== "." }));
-
-            // if (findWord.every((el) => { el !== "." })) { 
-            //     return winner(); 
-            // }
-
-
 
         }
         else {
             pieces[attemp].classList.remove("d-none");//
             attemp++;
+
             gameOver(attemp)//invoke gameOver function to show game over word
         }
+
         wordId.innerHTML = findWord.join("");
     })
+
 }
 
 function gameOver(num) {// gameOver function
-    console.log(num, "num");
     if (num == 4) {
-        console.log("oyun bitti");
         go.classList.remove("dispImg");
         tryagain.classList.remove("dispImg");//gameOver funtion also shows reload section on screen
-        tryagain.addEventListener("click", () => { location.reload() })
+        tryagain.addEventListener("click", () => { location.reload() });
 
     }
-
     return;
 }
 
